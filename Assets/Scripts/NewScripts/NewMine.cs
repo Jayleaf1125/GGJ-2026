d38ed59d678ecaac159d14310aba12ef07346589
EReyes13 <itsmeelio13@gmail.com>
2026-02-07 02:20:55 -0500

    Update 2
    All I was able to finish was the Miner character. I was also begin to create the concept of a function that calls at the end of round. This started to check if the miner struck gold, but I may use this to update leaderboards and such.

---
+++ Assets/Scripts/NewScripts/NewMine.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class NewMine : MonoBehaviour
+{
+   public NewScore Score;
+   //this float tracks the chance that the miner has of striking gold. This float will be compared to whatever number rolled when it comes time to it
+   // (specifically, at the end of a round)
+   public float GoldProb = 0;
+   
+   //this bool checks if the Miner made a deal with the con man
+   public bool Conned = false;
+
+   //this number will be converted to a random number between 1 and 100, which will then be compared to the gold probability to decide if the miner strikes gold
+    public float GoldDecider = 0;
+    void Start()
+    {
+        Score = GetComponent<NewScore>();
+    }
+
+    public void Deal(GameObject Partner)
+    {
+        if(Partner == GameObject.Find("ConMan"))
+        {
+            Conned = true;
+        }
+        else
+        {
+            //this just increases the chances of striking gold by 5 percent i think
+            GoldProb += 5;
+            Score.AddMoney(1);
+        }
+    }
+
+    //I might implement this function to all of the other characters. This function is the first time I am adding it/conceptualizing it.
+    //  This may be useful since I would have to display scores at end of Round, but I can probably just do that on the score manager script instead.  
+    public void EndRound()
+    {
+        GoldDecider = Random.Range(1, 100);
+        if(GoldDecider < GoldProb)
+        {
+            Score.AddMoney(2);
+        }
+    }
+    
+}
